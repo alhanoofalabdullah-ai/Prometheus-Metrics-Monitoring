d994a0dbb626dd337f6baa2e8cf79460458254c6
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 14:24:27 +0300

    Create OpenTelemetryConfig.java

---
+++ project-19-enterprise-observability-platform/OpenTelemetryConfig.java
@@ -0,0 +1,9 @@
+package com.alhanoof.observability;
+
+public class OpenTelemetryConfig {
+
+    public void initializeTracing() {
+
+        System.out.println("OpenTelemetry Initialized");
+    }
+}
