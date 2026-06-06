3962cdc6c030e32c96e84cf231dc861bbb80ddd0
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-06 07:25:57 +0300

    Create ApiMonitoringApplication.java

---
+++ project-07-api-performance-monitoring/ApiMonitoringApplication.java
@@ -0,0 +1,12 @@
+package com.alhanoof.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMonitoringApplication.class, args);
+    }
+}
